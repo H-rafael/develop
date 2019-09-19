731343da86c821173638fde8c78ca35ebe1fa9f8
大花 <tamen2009@163.com>
2019-09-19 17:22:03 +0800

    re

--- register.php
+++ register.php
@@ -13,37 +13,108 @@ $bodyClass = 'body-100';
 
 include 'header.php';
 ?>
-<div class="typecho-login-wrap">
-    <div class="typecho-login">
-        <h1><a href="http://typecho.org" class="i-logo">Typecho</a></h1>
-        <form action="<?php $options->registerAction(); ?>" method="post" name="register" role="form">
-            <p>
-                <label for="name" class="sr-only"><?php _e('用户名'); ?></label>
-                <input type="text" id="name" name="name" placeholder="<?php _e('用户名'); ?>" value="<?php echo $rememberName; ?>" class="text-l w-100" autofocus />
-            </p>
-            <p>
-                <label for="mail" class="sr-only"><?php _e('Email'); ?></label>
-                <input type="email" id="mail" name="mail" placeholder="<?php _e('Email'); ?>" value="<?php echo $rememberMail; ?>" class="text-l w-100" />
-            </p>
-            <p class="submit">
-                <button type="submit" class="btn btn-l w-100 primary"><?php _e('注册'); ?></button>
-            </p>
-        </form>
-        
-        <p class="more-link">
-            <a href="<?php $options->siteUrl(); ?>"><?php _e('返回首页'); ?></a>
-            &bull;
-            <a href="<?php $options->adminUrl('login.php'); ?>"><?php _e('用户登录'); ?></a>
-        </p>
+
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="Start your development with a Dashboard for Bootstrap 4.">
+    <meta name="author" content="Creative Tim">
+</head>
+
+<body class="bg-default">
+
+<!-- Main content -->
+<div class="main-content">
+    <!-- Header -->
+    <div class="header bg-gradient-primary py-7 py-lg-8 pt-lg-9">
+        <div class="container">
+            <div class="header-body text-center mb-7">
+                <div class="row justify-content-center">
+                    <div class="col-xl-5 col-lg-6 col-md-8 px-5">
+                        <h1 class="text-white">Typecho</h1>
+                        <p class="text-lead text-white">红墙白雪你说曾说过喜欢，我很喜欢。红墙白雪，要你喜欢。</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="separator separator-bottom separator-skew zindex-100">
+            <svg x="0" y="0" viewBox="0 0 2560 100" preserveAspectRatio="none" version="1.1" xmlns="http://www.w3.org/2000/svg">
+                <polygon class="fill-default" points="2560 0 2560 100 0 100"></polygon>
+            </svg>
+        </div>
+    </div>
+    <!-- Page content -->
+    <div class="container mt--8 pb-5">
+        <div class="row justify-content-center">
+            <div class="col-lg-5 col-md-7">
+                <div class="card bg-secondary border-0">
+                    <div class="card-header bg-transparent pb-5">
+                        <div class="text-muted text-center mt-2 mb-4"><small>Typecho 后台注册</small></div>
+                        <div class="text-center">
+                            <a href="<?php $options->siteUrl(); ?>" class="btn btn-neutral btn-icon mr-4">
+                                <span class="btn-inner--icon"><i class="ni ni-send"></i></span>
+                                <span class="btn-inner--text"><?php _e('返回首页'); ?></span>
+                            </a>
+                            <a href="<?php $options->adminUrl('login.php'); ?>" class="btn btn-neutral btn-icon">
+                                <span class="btn-inner--icon"><i class="ni ni-shop"></i></span>
+                                <span class="btn-inner--text"><?php _e('用户登录'); ?></span>
+                            </a>
+                        </div>
+                    </div>
+                    <div class="card-body px-lg-5 py-lg-5">
+                        <form action="<?php $options->registerAction(); ?>" method="post" name="register" role="form">
+                            <div class="form-group">
+                                <div class="input-group input-group-merge input-group-alternative mb-3">
+                                    <div class="input-group-prepend">
+                                        <span class="input-group-text"><i class="ni ni-hat-3"></i></span>
+                                    </div>
+                                    <input type="text" id="name" name="name" placeholder="<?php _e('用户名'); ?>" value="<?php echo $rememberName; ?>" class="form-control" autofocus />
+                                </div>
+                            </div>
+                            <div class="form-group">
+                                <div class="input-group input-group-merge input-group-alternative mb-3">
+                                    <div class="input-group-prepend">
+                                        <span class="input-group-text"><i class="ni ni-email-83"></i></span>
+                                    </div>
+                                    <input type="email" id="mail" name="mail" placeholder="<?php _e('Email'); ?>" value="<?php echo $rememberMail; ?>" class="form-control" />
+                                </div>
+                            </div>
+                            <div class="text-center">
+                                <button type="submit" class="btn btn-primary mt-4"><?php _e('注册'); ?></button>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
 </div>
-<?php 
+<!-- Footer -->
+<footer class="py-5" id="footer-main">
+    <div class="container">
+        <div class="row align-items-center justify-content-xl-between">
+            <div class="col-xl-6">
+                <div class="copyright text-center text-xl-left text-muted">
+                    Copyright ©  2019 qqexit.com Limited All Rights Reserved.
+                    <a href="http://qqexit.com/" class="font-weight-bold ml-1" target="_blank"> Kiln</a>
+                </div>
+            </div>
+
+        </div>
+    </div>
+</footer>
+</body>
+</html>
+<?php
 include 'common-js.php';
 ?>
 <script>
-$(document).ready(function () {
-    $('#name').focus();
-});
+    $(document).ready(function () {
+        $('#name').focus();
+    });
 </script>
 <?php
 include 'footer.php';
